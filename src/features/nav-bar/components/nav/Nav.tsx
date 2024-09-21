78ff2fe542c657a7b88a484f3b783011ef790f5f
santy <santy@DESKTOP-7KIGB8O>
2024-09-21 17:03:48 -0400

    added hamburger menu

--- src/features/nav-bar/components/nav/Nav.tsx
+++ src/features/nav-bar/components/nav/Nav.tsx
@@ -2,19 +2,76 @@
 import styles from "./Nav.module.css";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import Image from "next/image";
+import Hamburger from "hamburger-react";
+import { useState } from "react";
 
 const Nav = () => {
     const path = usePathname();
-    console.log(path);
+    const [isOpen, setOpen] = useState(false);
 
     return (
         <nav className={styles.navContainer}>
             <section className={styles.textContainer}>
-                <div className={styles.navItem}>
-                    <Link href="/">
-                        <p className={styles.companyName}>S&P</p>
-                    </Link>
-                </div>
+                <Link href="/" className={styles.logoContainer}>
+                    <Image
+                        className={styles.logo}
+                        alt="logo"
+                        src="/images/logos/main-logo-banner.png"
+                        width={300}
+                        height={100}
+                        quality={100}
+                        priority
+                    />
+                </Link>
+
+                <section className={styles.hamburgerMenuContainer}>
+                    <Hamburger toggled={isOpen} toggle={setOpen} />
+                    {isOpen && (
+                        <section className={styles.hamburgerMenu}>
+                            <Link href="/">
+                                <p
+                                    className={`${styles.hamNavLink} ${
+                                        path.length === 1
+                                            ? styles.active
+                                            : styles.inactive
+                                    }`}
+                                >
+                                    Home
+                                </p>
+                            </Link>
+                            <Link href="/works">
+                                <p
+                                    className={`${styles.hamNavLink} ${
+                                        path.includes("works")
+                                            ? styles.active
+                                            : styles.inactive
+                                    }`}
+                                >
+                                    Works
+                                </p>
+                            </Link>
+                            <Link href="/contact">
+                                <p
+                                    className={`${styles.hamNavLink} ${
+                                        path.includes("contact")
+                                            ? styles.active
+                                            : styles.inactive
+                                    }`}
+                                >
+                                    Contact
+                                </p>
+                            </Link>
+                            <Link
+                                href="/contact"
+                                className={styles.hamNavButton}
+                            >
+                                <p>Let&apos;s Meet!</p>
+                            </Link>
+                        </section>
+                    )}
+                </section>
+
                 <div className={styles.navItem}>
                     <Link href="/">
                         <p
